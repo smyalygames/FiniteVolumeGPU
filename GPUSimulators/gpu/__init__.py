d4f2ffc49369a41314e3f61af1f93bc67b619d65
Anthony Berg <smyalygames@gmail.com>
2025-06-24 19:57:59 +0200

    feat(kernel): add environment condition for importing certain packages

--- GPUSimulators/gpu/__init__.py
+++ GPUSimulators/gpu/__init__.py
@@ -1,2 +1,8 @@
-from .cuda_context import CudaContext
-from .hip_context import HIPContext
+from os import environ
+
+__env_name = 'GPU_LANG'
+
+if __env_name in environ and environ.get(__env_name).lower() == "cuda":
+    from .cuda_context import CudaContext as KernelContext
+else:
+    from .hip_context import HIPContext as KernelContext
