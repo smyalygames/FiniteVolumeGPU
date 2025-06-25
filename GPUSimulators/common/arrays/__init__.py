c549556c36756f226eb4a3e15d27a50348900f80
Anthony Berg <smyalygames@gmail.com>
2025-06-25 13:12:55 +0200

    feat(common): add imports for HIP 2d arrays

--- GPUSimulators/common/arrays/__init__.py
+++ GPUSimulators/common/arrays/__init__.py
@@ -1,11 +1,10 @@
 from os import environ
 
-# TODO this is temporary, remove
-from .cuda import *
-
 __env_name = 'GPU_LANG'
 
 if __env_name in environ and environ.get(__env_name).lower() == "cuda":
     from .cuda import *
 else:
     from .hip import *
+    # TODO this is temporary, remove
+    from .cuda import array3d
