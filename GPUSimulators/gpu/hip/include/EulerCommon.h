fa3fcb76f83824d83453e363e43a1693b75f49cb
Anthony Berg <smyalygames@gmail.com>
2025-07-03 13:11:57 +0200

    feat(gpu): remove breaking include in header

--- GPUSimulators/gpu/hip/include/EulerCommon.h
+++ GPUSimulators/gpu/hip/include/EulerCommon.h
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-#include <hip/amd_detail/amd_hip_vector_types.h>
 #include <hip/hip_runtime.h>
 #include "common.h"
 #include "limiters.h"
