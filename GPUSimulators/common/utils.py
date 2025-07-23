320daff96dd9d004b3908543ef1f7113de3d534d
Anthony Berg <smyalygames@gmail.com>
2025-07-23 12:11:20 +0200

    fix(common): import type for function in util

--- GPUSimulators/common/utils.py
+++ GPUSimulators/common/utils.py
@@ -1,9 +1,9 @@
+from pathlib import Path
+
 def get_project_root() -> Path:
     """
     Gets the root directory of the project.
     """
-    from pathlib import Path
-
     return Path(__file__).parent.parent.parent
 
 
@@ -26,3 +26,24 @@ def get_includes(file: str, local: bool = False) -> list[str]:
         pattern = '^\\W*#include\\s+"(.+?)"\\W*$'
 
     return re.findall(pattern, file, re.M)
+
+def unique_file(filename: str) -> str:
+    """
+    Finds a unique name for a filename. Will append a padded 4-digit number to the
+    tail of the stem of the file name.
+
+    Args:
+        filename: The filename to find a unique name.
+
+    Returns:
+        A unique filename.
+    """
+    import os
+
+    i = 0
+    stem, ext = os.path.splitext(filename)
+    while os.path.isfile(filename):
+        filename = f"{stem}_{str(i).zfill(4)}{ext}"
+        i = i + 1
+
+    return filename
